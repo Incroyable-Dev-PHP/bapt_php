5ea76d15696c867074de7ddcfc62c7fe4c073e70
BardenoED <116876174+BardenoED@users.noreply.github.com>
2022-11-07 16:53:04 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-
-    <!-- header included here -->
-    <!-- ... -->
+<html lang="fr">
+<?php
+    include 'includes\head.html';
+    include 'includes\Header.html';
+    include 'includes\ul.html';
+    ?>
 
     <body>
         <div class="container">
-            <?php
-                // your code here with conditions
-                // ...
+        <?php
+                if (isset($_GET['about-me'])) {
+                    include './pages/about-me.html';
+                } elseif (isset($_GET['my-dreams'])) {
+                    include './pages/my-dreams.html';
+                } elseif (isset($_GET['my-passions'])) {
+                    include './pages/my-passions.html';
+                } else {
+                    include './pages/home.html';
+                }
             ?>
         </div>
     </body>
-
+    <?php
+    include 'includes\footer.html';
+    ?>
 </html>
